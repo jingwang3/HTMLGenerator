7b3b61dcfab96932a5b15c8adb16a8b4b527defe
jingwang3 <jingwang3@gmail.com>
2014-03-03 12:27:36 -0500

    use jquery plug-in

---
+++ plug-in.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+  	$('.jqte-test').jqte();
+	
+	// settings of status
+	var jqteStatus = true;
+	
+});
+	
+$(".status").click(function()
+	{
+		jqteStatus = jqteStatus ? false : true;
+		$('.jqte-test').jqte({"status" : jqteStatus})
+	}
+);
