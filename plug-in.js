f43aee5a6c8a2eaaae7a4eebda48b42b7a29870d
jingwang3 <jingwang3@gmail.com>
2014-03-03 14:26:57 -0500

    dynamic fields

--- plug-in.js
+++ plug-in.js
@@ -1,14 +1,23 @@
+var jqteStatus = true;
+var bodyContent = '<textarea name="textarea" class="jqte-test"></textarea>';
+
 $( document ).ready(function() {
   	$('.jqte-test').jqte();
-	
-	// settings of status
-	var jqteStatus = true;
-	
+  	$(".status").click(function()
+		{
+			jqteStatus = jqteStatus ? false : true;
+			$('.jqte-test').jqte({"status" : jqteStatus})
+		}
+	);
+
+	$("#updateBtn").click(function()
+		{
+			var content = '';
+			for (var i = 0; i < $('#paraNum').val(); i++) {
+				content += bodyContent;
+			};
+			$('#content-box').html(content);
+			$(".jqte-test").jqte();
+		}
+	);
 });
-	
-$(".status").click(function()
-	{
-		jqteStatus = jqteStatus ? false : true;
-		$('.jqte-test').jqte({"status" : jqteStatus})
-	}
-);
