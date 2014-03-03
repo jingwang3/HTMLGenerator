d774a2f720a5dc9322df60a2f21fd2c4dc16501d
jingwang3 <jingwang3@gmail.com>
2014-03-03 18:32:53 -0500

    html generate btn

--- plug-in.js
+++ plug-in.js
@@ -2,22 +2,45 @@ var jqteStatus = true;
 var bodyContent = '<textarea name="textarea" class="jqte-test"></textarea>';
 
 $( document ).ready(function() {
-  	$('.jqte-test').jqte();
+	//toggle html and editor
   	$(".status").click(function()
 		{
-			jqteStatus = jqteStatus ? false : true;
-			$('.jqte-test').jqte({"status" : jqteStatus})
+			if(jqteStatus){
+				jqteStatus = false;
+				$('.jqte-test').jqte({"status" : false});
+			}else{
+				jqteStatus = true;
+				$('.jqte-test').jqte({"status" : true});
+			}
+			
 		}
 	);
-
+  	//generate paragraph containers
 	$("#updateBtn").click(function()
 		{
-			var content = '';
-			for (var i = 0; i < $('#paraNum').val(); i++) {
-				content += bodyContent;
-			};
-			$('#content-box').html(content);
-			$(".jqte-test").jqte();
+			if($('#paraNum').val() >0 && $('#paraNum').val()<=15){
+				var content = '';
+				for (var i = 0; i < $('#paraNum').val(); i++) {
+					content += "<h2>Paragraph " + (i+1) + "</h2>" + bodyContent;
+				};
+				$('#content-box').html(content);
+				$(".jqte-test").jqte();
+				jqteStatus = true;
+			}
+		}
+	);
+	//generate html
+	$("#export").click(function()
+		{
+			var content = "";
+			if(jqteStatus){
+				$('.jqte-test').jqte({"status" : false});
+				jqteStatus = false;
+			}
+			$(".jqte-test").each(function() {
+				content += ('<div>' + $(this).text() + '</div><br><hr>');
+    			$('.html-code').text(content);
+			})
 		}
 	);
 });
